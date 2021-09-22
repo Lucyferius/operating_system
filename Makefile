4a4edf200a4974c15fcda2602146a48f0a60d08a
Lucyferius <lusyag23@gmail.com>
2021-09-22 22:42:59 +0300

    initial commit - game bandit

--- Makefile
+++
@@ -1,5 +0,0 @@
-compile: 
-	gcc input.c print_answer.c logic.c main.c -o ./app.out
-	
-run: 
-	./app.out	
--- input.h
+++
@@ -1,8 +0,0 @@
-#ifndef USER_INPUT_H_   /* Include guard */
-#define USER_INPUT_H_
-
-int enter_bet(int budget);  /* function declaration */
-int enter_budget();
-int choose_continue ();
-
-#endif // USER_INPUT_H_
